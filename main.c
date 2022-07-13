38abcb10150d1ceab6f0aba123bb344cdcfc42f4
hichem <hichemmaatoug55@gmail.com>
2022-07-13 10:24:46 -0700

    main function

---
+++ main.c
@@ -0,0 +1,82 @@
+#include "monty.h"
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: int
+**/
+int main(int argc, char *argv[])
+{
+	FILE *fi;
+	char *s = NULL;
+	size_t line_buffer_size = 0;
+	char *c = NULL;
+	int n = 0, i = 0, j = 0, x = 1;
+	stack_t *str = NULL;
+	instruction_t st_fn[] = {{"push", push_pall},
+							{"pall", pall_push},
+							{"pint", printe},
+							{"pop", pop},
+							{"swap", swap},
+							{"add", add},
+							{"nop", nop},
+							{"sub", sub},
+							{"div", div},
+							{"mul", mul},
+							{"mod", mod},
+							{"#", ha},
+							{"pchar", pchar},
+							{NULL, NULL}};
+
+	if (argc != 2) 
+		{
+			fprintf(stderr, "USAGE: monty file\n");
+			exit(EXIT_FAILURE);
+		}
+	fi = fopen(argv[1], "r");
+	if (!fi)
+		{
+			fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+	while (!feof(fi))
+	{
+		x = 1;
+		i = 0;
+		while (x == 1)
+		{
+			x = getline(&s, &line_buf_size, fi);
+			j++;
+		}
+		if (x < 0)
+			break;
+		n = toke2(s);
+		c = toke1(s);
+		if (c)
+		if ((n == 6666 && strcmp(c, st_fn[0].opcode) == 0))
+		{
+			fprintf(stderr, "L%i: usage: push integer\n", j);
+			free_z(str);
+			free(s);
+			exit(EXIT_FAILURE);
+		}
+		while ((st_fn[i].opcode != NULL) && (strcmp(st_fn[i].opcode, c) != 0))
+		{
+			i++;
+		}
+		if (!(st_fn[i].opcode))
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", j, c);
+			free_z(str);
+			free(s);
+			exit(EXIT_FAILURE);
+		}
+		if (i > 0)
+			n = j;
+		st_fn[i].f(&str, n);
+	}
+	free_z(str);
+	free(s);
+	fclose(fi);
+	return (0);
+}
